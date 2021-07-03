661b9d9db6ba9cf9f6ca9cd0e068f0c4519beec3
zaixiamaomaoyu <1143962543@qq.com>
2021-07-03 09:41:52 +0800

    '初始化项目'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="tab-bar">
+      <div class="tab-bar-item">
+        <div>首页</div>
+      </div>
+      <div class="tab-bar-item">
+        <div>分类</div>
+      </div>
+      <div class="tab-bar-item">
+        <div>购物车</div>
+      </div>
+      <div class="tab-bar-item">
+        <div>我的</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'assets/css/base.css'
+
+  
 </style>
