eaf02f3ee1cc83a853489043ac0e7e3251e35ebb
zaixiamaomaoyu <1143962543@qq.com>
2021-07-11 10:56:39 +0800

    '初始化项目'

--- src/network/request.js
+++ src/network/request.js
@@ -1,49 +1,9 @@
 import axios from "axios";
-
-// export function request(config,success,failure) {
-//     const instance = axios.create({
-//         baseURL:'http://123.207.32.32:8000',
-//         timeout:5000
-//     })
-
-//     // 发送真正的网络请求
-//     instance(config)
-//     .then(res => {
-//         // console.log(res);
-//         success(res)
-//     })
-//     .catch(err => {
-//         // console.log(err);
-//         failure(err)
-//     })
-// }
-
-// export function request(config) {
-//     return new Promise((resolve,reject) => {
-//         // 1.创建axios的实例
-//         const instance = axios.create({
-//             baseURL:'http://123.207.32.32:8000',
-//             timeout:5000
-//         })
-    
-//         // 发送真正的网络请求
-//         instance(config)
-//         .then(res => {
-//             // console.log(res);
-//             resolve(res)
-//         })
-//         .catch(err => {
-//             // console.log(err);
-//             reject(err)
-//         })
-//     })
-// }
-
 export function request(config) {
    
         // 1.创建axios的实例
         const instance = axios.create({
-            baseURL:'http://123.207.32.32:8000',
+            baseURL:"http://152.136.185.210:7878/api/m5" ,
             timeout:5000
         })
     
@@ -62,21 +22,5 @@ export function request(config) {
         })
 
         // 3.发送真正的网络请求
-        return instance(config)
-
-        
-        
+        return instance(config)      
 }
-
-
-
-// function test(aaa,bbb) {
-//     aaa('hello world'),
-//     bbb('err messages')
-// }
-
-// test(function(res) {
-//     console.log(res);
-// },function(err) {
-//     console.log(err);
-// })
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,55 +1,159 @@
 <template>
-    <div  id="home">
+    <div id="home">
         <nav-bar class="home-nav">
             <div slot="center">购物街</div>
         </nav-bar>
-        <swiper>
-            <swiper-item v-for="(item, index) in bannars" :key=" index">
-                <a :href="item.link">
-                    <img :src="item.image" alt="">
-                </a>
-            </swiper-item>
-        </swiper>
-    </div>
+        
+        <scroll class="content">
+            <home-swiper :banners = 'banners'></home-swiper>  <!--传父组件的值出去-->
+            <recommend-view :recommends="recommends"></recommend-view>
+            <feature-view/>
+            <tab-control :titles="['流行', '新款', '精选']" 
+            class="tab-control" 
+            @tabClick='tabClick'></tab-control>
+            <goods-list :goods="showGoods"></goods-list>
+        </scroll>
+
+    </div> 
     
 </template>
 
 <script>
+import HomeSwiper from './childComps/HomeSwiper.vue'  //导入子组件
+import RecommendView from './childComps/RecommendView.vue'
+import FeatureView from './childComps/FeatureView.vue'
 
-import {getHomeMultidata} from 'network/home'
-import NavBar from 'components/common/navbar/NavBar.vue';
-import {Swiper, SwiperItem} from 'components/common/swiper'
+import NavBar from 'components/common/navbar/NavBar';
+import TabControl from 'components/content/tabcontrol/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll'
 
+import {getHomeMultidata, getHomeGoods} from 'network/home'
 
 export default {
    name:'Home',
    components:{
+        HomeSwiper,
+        RecommendView,
+        FeatureView,
         NavBar,
-        // getHomeMultidata,
-        Swiper,
-        SwiperItem,
-      
+        TabControl,
+        GoodsList,
+        Scroll
    },
   
     data() {
-        return {
-            bannars:[],
-            recommoends:[]
+       return {
+            banners:[],
+            recommends:[],
+            goods: {
+                'pop': {page: 0, list: []},
+                'new': {page: 0, list: []},
+                'sell': {page: 0, list: []},
+            },
+            currentType:'pop'
         }
     },
+    computed: {
+        showGoods() {
+            return this.goods[this.currentType].list
+        }
+    },
+
    created(){
-       getHomeMultidata().then(res => {
-            // console.log(res);
-            this.bannars = res.data.bannar
-            this.recommoends = res.data.recommoend
-       })
+       this.getHomeMultidata()
+
+    //    请求商品数据
+        this.getHomeGoods('pop')
+        this.getHomeGoods('new')
+        this.getHomeGoods('sell')     
+   },
+
+
+   methods: {
+    //    事件监听的相关方法
+    tabClick(index) {
+        switch(index) {
+            case 0:
+                this.currentType = 'pop'
+                break
+            case 1:
+                this.currentType = 'new'
+                break
+            case 2:
+                this.currentType = 'sell'
+                break
+        }
+    },
+
+    //    网络请求的相关方法
+       getHomeMultidata() {
+           getHomeMultidata().then(res => {
+                // console.log(res);
+                this.banners = res.data.banner.list;
+                this.recommends = res.data.recommend.list
+            })
+       },
+
+        // getHomeGoods(type) {
+        //     const page = this.goods[type].page + 1
+        //     getHomeGoods(type, page).then(res => {
+        //         const newList = res.data.list
+        //         this.goods[type].list.push(...newList)
+        //         this.goods[type].page += 1
+        //     })
+        // }
+
+        getHomeGoods(type) {
+            const page = this.goods[type].page + 1
+            getHomeGoods(type, page).then(res => {
+                this.goods[type].list.push(...res.data.list)
+                this.goods[type].page += 1
+            })
+        }
    }
+
+
 };
 </script>
 
 <style scoped>
+/* scope作用域   */
+    #home {
+        padding-top: 44px;
+        height: 100vh;
+        position: relative;
+    }
+
     .home-nav {
         background-color: var(--color-tint);
         color: #fff;
+
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 9;
     }
+
+    .tab-control {
+        position: sticky;
+        top:43px;
+        z-index: 9;
+    }
+
+    .content {
+        overflow: hidden;
+
+        position: absolute;
+        top: 44px;
+        bottom: 49px;
+        left: 0;
+        right: 0;
+    }
+    /* .content {
+        height: calc(100% - 93px);
+        overflow: hidden; 
+
+    } */
 </style>
